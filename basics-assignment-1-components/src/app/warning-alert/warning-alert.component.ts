bc1042f66c0419191dc4642b3e9cb9245de67288
George Katsanos <katsano.george@gmail.com>
2023-01-06 22:00:16 +0200

    Basics - Assignment 1 - Components

---
+++ basics-assignment-1-components/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styles: [`
+    div {
+      color: red;
+    }
+  `]
+})
+
+export class WarningAlertComponent {}

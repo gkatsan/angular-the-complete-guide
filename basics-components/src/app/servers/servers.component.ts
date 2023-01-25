cbf4c0a3fc723aafa8563ee4eac1ef8df1227dc2
George Katsanos <katsano.george@gmail.com>
2023-01-25 23:20:23 +0200

    Basics - Data Binding

--- basics-components/src/app/servers/servers.component.ts
+++ basics-components/src/app/servers/servers.component.ts
@@ -8,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
+  server2Name = 'Test server';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 3000);
+  }
 
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: Event) {
+    // console.log('event:', event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
